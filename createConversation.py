d9251c706d2001adbba088b4b10d9a724792b4b5
Suhail Ahamed <suhailahamed@Suhails-MacBook-Pro.local>
2018-12-28 23:40:41 +0400

    create mediator, startConversation creates 10 lines of conversation

---
+++ createConversation.py
@@ -0,0 +1,20 @@
+# import tensorflow as tf
+# import numpy as np
+# import sys
+# from random import randint
+# import datetime
+# from sklearn.utils import shuffle
+# import pickle
+# import os
+# import time
+
+import SonResponse as son
+import FatherResponse as father
+
+sonsResponse = son.response("Hi")
+print("Son :" , sonsResponse)
+for i in range(10):
+    fathersResponse = father.response(sonsResponse)
+    print("Father :" , fathersResponse)
+    sonsResponse = son.response(fathersResponse)
+    print("Son :" , sonsResponse)
